8b6e29fd72d3b7ac36367f1a48de44807e434474
AngelDev727 <rapanjel@hotmail.com>
2023-03-12 23:49:13 +0000

    remove useless code

--- src/App.js
+++ src/App.js
@@ -14,30 +14,27 @@ function App() {
     "https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
     "https://storage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
-  ]
+  ];
 
-  const [index, setIndex] = useState(0)
-  // const [url, setUrl] = useState(videos[index]);
+  const [index, setIndex] = useState(0);
+
+  const handleChangeVideo = () => {
+    setIndex(index === videos.length - 1 ? 0 : index + 1);
+  };
 
   return (
     <div className="App">
       <ReactPlayer
         className="videoPlayer"
         ref={playerRef}
-        // url={url}
         url={videos[index]}
         playing={true}
-        // height="auto"
         width="100%"
       />
       <div className="videoLink">
         Video link: <a href={videos[index]} target="_blank" rel="noreferrer">{videos[index]}</a>
       </div>
-
-      <div
-        className="butChange"
-        onClick={() => setIndex(index === videos.length - 1 ? 0 : index + 1)}
-      >
+      <div className="butChange" onClick={handleChangeVideo}>
         Change video
       </div>
     </div>
